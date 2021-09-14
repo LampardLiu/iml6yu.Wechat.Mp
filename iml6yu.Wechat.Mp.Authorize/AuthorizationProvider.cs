9745b2ab9f67e5b1e8738bcebdb49eef11da0b07
Iml6yu <646007589@qq.com>
2021-09-15 01:24:15 +0800

    添加获取accesstoken,暂时放这里，需要整里，这个不合规矩

--- iml6yu.Wechat.Mp.Authorize/AuthorizationProvider.cs
+++ iml6yu.Wechat.Mp.Authorize/AuthorizationProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -38,11 +40,45 @@ namespace iml6yu.Wechat.Mp.Authorization
             return (await GetAccessTokenAsync(code))?.OpenId ?? "error:no openid";
         }
 
+       
         private async Task<AccessTokenModel> GetAccessTokenAsync(string code)
         {
             var content = await (await new HttpClient().GetAsync($"https://api.weixin.qq.com/sns/oauth2/access_token?appid={appid}&secret={appsecret}&code={code}&grant_type=authorization_code")).Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<AccessTokenModel>(content);
-            return result; 
+            return result;
+        }
+
+        public async Task<AccessTokenModel> GetClientAccessTokenAsync(string appid,string secret)
+        {
+            var content = await (await new HttpClient().GetAsync($"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={secret}")).Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<AccessTokenModel>(content);
+            return result;
+        }
+
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <param name="access_token"></param>
+        /// <param name="openid"></param>
+        /// <returns></returns>
+        public  WxUserInfo GetWxUserInfo(string access_token, string openid)
+        { 
+            var wxUserInfo_Url = string.Format("https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang=zh_CN", access_token, openid);
+            var result = new  WxUserInfo();
+
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var get_result = wc.DownloadString(wxUserInfo_Url);
+        
+            if (!get_result.Contains("error"))
+            {
+                var wxApi_ResultModel = JsonConvert.DeserializeObject<WxUserInfo>(get_result);
+                result = wxApi_ResultModel;
+            }
+            else
+                throw new Exception(string.Format("wx获取用户信息接口错误，error：{0}", "get_result"));
+
+            return result;
         }
     }
 }
---
+++ iml6yu.Wechat.Mp.Authorize/WxUserInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iml6yu.Wechat.Mp.Authorization
+{
+    public class WxUserInfo
+    {
+
+        public int? subscribe { get; set; }
+
+        public string openid { get; set; }
+
+        public string nickname { get; set; }
+
+        public int? sex { get; set; }
+
+        public string language { get; set; }
+
+        public string city { get; set; }
+
+        public string province { get; set; }
+
+        public string country { get; set; }
+
+        public string headimgurl { get; set; }
+
+        public long? subscribe_time { get; set; }
+
+        public string unionid { get; set; }
+
+        public string remark { get; set; }
+
+        public string groupid { get; set; }
+
+        public List<int> tagid_list { get; set; }
+
+        public string subscribe_scene { get; set; }
+
+        public string qr_scene { get; set; }
+
+        public string qr_scene_str { get; set; }
+
+        public string access_token { get; set; }
+
+    }
+}
